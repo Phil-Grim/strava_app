c4d5eb9c902d005cb1195626741adc9a575cc782
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-28 12:23:24 +0000

    Update strava.py

--- strava.py
+++ strava.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+APP_URL = https://stravaapp-5jhpgdn9kfmhmkdy5d5yrs.streamlit.app/
+STRAVA_CLIENT_ID = o106698
+STRAVA_CLIENT_SECRET = a66bb23d9597d2b4027ff22a73720f226d021f68
+STRAVA_AUTHORIZATION_URL = "https://www.strava.com/oauth/authorize"
+STRAVA_API_BASE_URL = "https://www.strava.com/api/v3"
+DEFAULT_ACTIVITY_LABEL = "NO_ACTIVITY_SELECTED"
+
+
 def header():
     col1, col2, col3 = st.columns(3)
 
@@ -8,6 +16,42 @@ def header():
 
     return col1, col2, col3, strava_button
 
+def authorization_url():
+    request = httpx.Request(
+        method="GET",
+        url=STRAVA_AUTHORIZATION_URL,
+        params={
+            "client_id": STRAVA_CLIENT_ID,
+            "redirect_uri": APP_URL,
+            "response_type": "code",
+            "approval_prompt": "auto",
+            "scope": "activity:read_all"
+        }
+    )
+
+    return request.url
+
+def login_header(header=None):
+    strava_authorization_url = authorization_url()
+
+    if header is None:
+        base = st
+    else:
+        col1, _, _, button = header
+        base = button
+
+    with col1:
+        powered_by_strava_logo()
+
+    base64_image = load_image_as_base64("./static/btn_strava_connectwith_orange@2x.png")
+    base.markdown(
+        (
+            f"<a href=\"{strava_authorization_url}\">"
+            f"  <img alt=\"strava login\" src=\"data:image/png;base64,{base64_image}\" width=\"100%\">"
+            f"</a>"
+        ),
+        unsafe_allow_html=True,
+    )
 
 def authenticate(header=None, stop_if_unauthenticated=True):
     query_params = st.experimental_get_query_params()
