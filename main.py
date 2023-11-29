a2d358891974484ddb0ca91a56aab03055efa6cd
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-29 12:29:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ strava_auth = strava.authenticate(header=strava_header, stop_if_unauthenticated=
 if strava_auth is None:
     st.markdown("Use the **Connect with Strava** button at the top of the screen to login!")
     st.warning('Please login!')
-    st.stop 
+    # st.stop
+    st.exception("Please login!")
 else:
     st.success("Thanks for logging in")
     json_activities = strava.get_activities(strava_auth)
