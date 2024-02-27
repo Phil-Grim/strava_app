a521c974852487c9a118777b8e49eedec957a821
PhilippeGrimshaw <philippegrimshaw10@gmail.com>
2024-02-27 13:04:22 +0000

    testing

--- strava/authenticate.py
+++ strava/authenticate.py
@@ -2,7 +2,18 @@ import streamlit as st
 import httpx
 import base64
 
-from strava.constants import *
+# from strava.constants import *
+APP_URL = "https://stravaapp-5jhpgdn9kfmhmkdy5d5yrs.streamlit.app/"
+
+
+STRAVA_CLIENT_ID = st.secrets["STRAVA_CLIENT_ID"]
+STRAVA_CLIENT_SECRET = st.secrets["STRAVA_CLIENT_SECRET"]
+
+
+STRAVA_AUTHORIZATION_URL = "https://www.strava.com/oauth/authorize"
+STRAVA_API_BASE_URL = "https://www.strava.com/api/v3"
+DEFAULT_ACTIVITY_LABEL = "NO_ACTIVITY_SELECTED"
+STRAVA_ORANGE = "#fc4c02"
 
 def header():
     '''adds 3 columns to the page, along with an empty container in the 3rd column'''
