c14fdd8ddb9cbe4b69db3e4c6364332cba6e1451
PhilippeGrimshaw <philippegrimshaw10@gmail.com>
2024-02-27 18:58:31 +0000

    amending authenticate

--- strava/authenticate.py
+++ strava/authenticate.py
@@ -126,7 +126,7 @@ def authentication(header=None):
 
     if authorisation_code is None:
         login_header(header=header)
-        st.stop()
+        return
     else:
         strava_auth = exchange_authorization_code(authorisation_code)
         logout_header(strava_auth, header=header)
@@ -162,20 +162,6 @@ def access_from_refresh(refresh_token):
     return access_token
 
 
-# @st.cache(show_spinner=False)
-# def get_activities(auth,page=1):
-#     access_token = auth["access_token"]
-#     response = httpx.get(
-#         url=f"{STRAVA_API_BASE_URL}/athlete/activities",
-#         params ={
-#             'per_page':200, 'page':page
-#         },
-#         headers={
-#             "Authorization": f"Bearer {access_token}",
-#         },
-#     )
-
-#     return response.json()
 
 
 ##### Take Json, add a slider and extract json into a cleaned table 
