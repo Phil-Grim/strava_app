cad3fc1dc5d6d68181ff6b48678c57c652347ec1
PhilippeGrimshaw <philippegrimshaw10@gmail.com>
2024-02-28 17:57:06 +0000

    histogram

--- main.py
+++ main.py
@@ -67,50 +67,51 @@ st.dataframe(
 # distance = strava.adding_headline_numbers(filtered_table)
 # st.write('You ran', distance, 'kms during the specified date range')
 
-    ##############################
-    ########### Adding Histogram - functionalise this in strava.py later
-    ##############################
-    
-    # from matplotlib.ticker import FormatStrFormatter # put here so I remember to move it into strava.py
-    # st.header("Run Distribution by Distance (kms)")
-    
-    # fig, ax = plt.subplots()
-    # ax.set_facecolor('#eafff5')
+#############################
+########## Adding Histogram - functionalise this in strava.py later
+#############################
 
-    # data = filtered_table['Distance (km)']
-    # w=5
-    # counts, bins, patches = ax.hist(data, bins=np.arange(0, max(data) + w, w), rwidth=1, facecolor='yellow', edgecolor='black')
+from matplotlib.ticker import FormatStrFormatter # put here so I remember to move it into strava.py
+import numpy as np
+st.header("Run Distribution by Distance (kms)")
 
-    # ax.set_xticks(bins)
-    # ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
-    # plt.xlabel('Kms per Run')
-    # plt.ylabel('Count')
-    # ax.xaxis.labelpad = 20
+fig, ax = plt.subplots()
+ax.set_facecolor('#eafff5')
 
-    # # Change the colors of bars at the edges...
-    # twentyfifth, seventyfifth = np.percentile(data, [25, 75])
-    # for patch, rightside, leftside in zip(patches, bins[1:], bins[:-1]): # the first rightside bin is at bins[1] and the last leftside patch is at bins[-1]
-    #     if rightside < twentyfifth:
-    #         patch.set_facecolor('green')
-    #     elif leftside > seventyfifth:
-    #         patch.set_facecolor('red')
+data = filtered_table['Kms']
+w=5
+counts, bins, patches = ax.hist(data, bins=np.arange(0, max(data) + w, w), rwidth=1, facecolor='yellow', edgecolor='black')
 
-    # # 2 lines to get y-coordinate for placement of raw count label
-    # max_height = max([x.get_height() for x in patches]) # height of tallest histogram bin
-    # label_placement = (max_height * 4/107) 
+ax.set_xticks(bins)
+ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.xlabel('Kms per Run')
+plt.ylabel('Count')
+ax.xaxis.labelpad = 20
+
+# Change the colors of bars at the edges...
+twentyfifth, seventyfifth = np.percentile(data, [25, 75])
+for patch, rightside, leftside in zip(patches, bins[1:], bins[:-1]): # the first rightside bin is at bins[1] and the last leftside patch is at bins[-1]
+    if rightside < twentyfifth:
+        patch.set_facecolor('green')
+    elif leftside > seventyfifth:
+        patch.set_facecolor('red')
+
+# 2 lines to get y-coordinate for placement of raw count label
+max_height = max([x.get_height() for x in patches]) # height of tallest histogram bin
+label_placement = (max_height * 4/107) 
+
+bin_centers = 0.5 * np.diff(bins) + bins[:-1]
+for count, x, patch in zip(counts, bin_centers, patches):
+    # Label the raw counts
+    ax.annotate(str(count), xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0,patch.get_height() + label_placement), textcoords=('offset points', 'data'), va='top', ha='center')
     
-    # bin_centers = 0.5 * np.diff(bins) + bins[:-1]
-    # for count, x, patch in zip(counts, bin_centers, patches):
-    #     # Label the raw counts
-    #     ax.annotate(str(count), xy=(x, 0), xycoords=('data', 'axes fraction'),
-    #         xytext=(0,patch.get_height() + label_placement), textcoords=('offset points', 'data'), va='top', ha='center')
-        
-    #     # Label the percentages
-    #     percent = '%0.0f%%' % (100 * float(count) / counts.sum())
-    #     ax.annotate(percent, xy=(x, 0), xycoords=('data', 'axes fraction'),
-    #         xytext=(0, -16), textcoords='offset points', va='top', ha='center')
-    
-    # st.pyplot(fig)
+    # Label the percentages
+    percent = '%0.0f%%' % (100 * float(count) / counts.sum())
+    ax.annotate(percent, xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0, -16), textcoords='offset points', va='top', ha='center')
+
+st.pyplot(fig)
 
     
 
