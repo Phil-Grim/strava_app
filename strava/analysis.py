f09a914abce5b4f27bb32129776336504a6193ba
PhilippeGrimshaw <philippegrimshaw10@gmail.com>
2024-02-28 20:25:20 +0000

    analysis update

--- strava/analysis.py
+++ strava/analysis.py
@@ -304,7 +304,7 @@ def activities_slider(activities):
 def create_dataframe(activities, refresh_token):
 
     rows = []
-    for i in activities[:160]:
+    for i in activities[:210]:
         activity_id = i["id"]
         activity_url = f'https://www.strava.com/activities/{activity_id}'
         name = i["name"]
